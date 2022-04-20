6fb343cd1bf2a16535471ce444437f22cec2c84e
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-20 14:40:03 +0530

    Photo Allignment

--- Js/about.js
+++ Js/about.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector(".team__card")
 
 const members = [
     {
-        image: "https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo=",
+        image: "https://profilemagazine.com/wp-content/uploads/2020/04/Ajmere-Dale-Square-thumbnail.jpg             ",
         name: "Hi, i'm John",
         occupation: "Founder and CEO",
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
@@ -14,13 +14,13 @@ const members = [
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     },
     {
-        image: "",
+        image: "https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg",
         name: "Hi, i'm Smith",
         occupation: "Marketing Execurive",
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     },
     {
-        image: "",
+        image: "https://l-ldesign.com.au/2016/wp-content/uploads/2020/01/profile-pic-katie-square.jpg",
         name: "Hi, i'm Natasha",
         occupation: "Senior Designer",
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
