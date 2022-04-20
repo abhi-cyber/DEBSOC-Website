cf5d1a8a85e9eb0623ba63b17331b35245db282b
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-20 15:12:06 +0530

    Hover Transition | Naming Correction

--- Js/about.js
+++ Js/about.js
@@ -3,25 +3,25 @@ const wrapper = document.querySelector(".team__card")
 const members = [
     {
         image: "https://profilemagazine.com/wp-content/uploads/2020/04/Ajmere-Dale-Square-thumbnail.jpg             ",
-        name: "Hi, i'm John",
+        name: "John",
         occupation: "Founder and CEO",
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
     },
     {
         image: "https://www.freecodecamp.org/news/content/images/size/w60/2022/02/adnan-min.jpeg",
-        name: "Hi, i'm Milan",
+        name: "Milan",
         occupation: "Lead Developer",
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
     },
     {
         image: "https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg",
-        name: "Hi, i'm Smith",
+        name: "Smith",
         occupation: "Marketing Execurive",
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
     },
     {
         image: "https://l-ldesign.com.au/2016/wp-content/uploads/2020/01/profile-pic-katie-square.jpg",
-        name: "Hi, i'm Natasha",
+        name: "Natasha",
         occupation: "Senior Designer",
         discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
     }
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             <div class="photo">
                 <img src = "${members[i].image}">
             </div>
-            <div class="title">${members[i].name}</div>
+            <div class="title">Hi, i'm <b>${members[i].name}</b></div>
             <div class="occu">${members[i].occupation}</div>
             <div class="desc">${members[i].discription}</div>
             <div class="links">
